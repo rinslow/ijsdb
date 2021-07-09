c038458b1db293afb3e1ebe846a7d6c62c47bca0
Dan <dan@monday.com>
2021-07-09 14:35:55 +0300

    Changed author
    bla

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
+import * as readline from 'readline';
+import * as chalk from 'chalk';
+
 export function setTrace(): void {
-  console.log("works")
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on('line', (line) => {
+    onLine(line);
+    rl.prompt()
+  });
+
+  rl.setPrompt(makePrompt());
+  rl.prompt()
+}
+
+function onLine(line: string): void {
+  console.log(`Got ${line}`);
+}
+
+/**
+ * generate the leading arrow in front of traceback or debugger
+ */
+function makePrompt(): string {
+  return chalk.green('ijsdb> ');
 }
